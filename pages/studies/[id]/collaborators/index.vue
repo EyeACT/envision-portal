4b1062a2a3dd03989f16dcd014f00a4ad6d46ff7
slugb0t <wheresdorian@gmail.com>
2025-02-04 17:29:02 -0800

    wip: :construction: add subpages for study management

---
+++ pages/studies/[id]/collaborators/index.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="p-6">
+    <h1 class="mb-4 text-2xl font-bold">Metadata</h1>
+    <!-- Metadata content or form goes here -->
+    <p>Here you can view or edit metadata related to your studies.</p>
+  </div>
+</template>
