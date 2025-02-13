b18d3fb031068949ec5029fa7074b3bbbd07f8a6
slugb0t <wheresdorian@gmail.com>
2025-02-12 20:39:04 -0800

    feat: :sparkles: type for study (frontend)

---
+++ types/study.d.ts
@@ -0,0 +1,12 @@
+export interface Study {
+  id: string;
+  title: string;
+  createdOn: string;
+  description: string;
+  image: string;
+  keywords: string[];
+  ownerId: string;
+  role: string;
+  updatedOn: string;
+  userName: string;
+}
