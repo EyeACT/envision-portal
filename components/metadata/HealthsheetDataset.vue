bd17b0d9547c3c53b391471579ac7db432824037
slugb0t <wheresdorian@gmail.com>
2025-03-03 12:39:03 -0800

    wip: :construction: creating health sheet content

--- components/metadata/HealthsheetDataset.vue
+++ components/metadata/HealthsheetDataset.vue
@@ -6,7 +6,67 @@ const props = defineProps<{
 </script>
 
 <template>
-  <div>metadata</div>
+  <div class="space-y-6">
+    <CardCollapsibleContent title="General Information">
+      <div>Testing content here</div>
+    </CardCollapsibleContent>
 
-  <pre>{{ metadata }}</pre>
+    <CardCollapsibleContent title="Dataset Versioning">
+      <div>Testing content here</div>
+    </CardCollapsibleContent>
+
+    <CardCollapsibleContent title="Motivation">
+      <div>Testing content here</div>
+    </CardCollapsibleContent>
+
+    <CardCollapsibleContent title="Data Composition">
+      <div>Testing content here</div>
+    </CardCollapsibleContent>
+
+    <CardCollapsibleContent
+      title="Devices and Contextual Attributes in Data Collection"
+    >
+      <div>Testing content here</div>
+    </CardCollapsibleContent>
+
+    <CardCollapsibleContent title="Challenge in tests and confounding factors">
+      <div>Testing content here</div>
+    </CardCollapsibleContent>
+
+    <CardCollapsibleContent
+      title="Collection and use of demograpohic information"
+    >
+      <div>Testing content here</div>
+    </CardCollapsibleContent>
+
+    <CardCollapsibleContent title="Pre-processing / De-Identification">
+      <div>Testing content here</div>
+    </CardCollapsibleContent>
+
+    <CardCollapsibleContent title="Labeling and subjectivity of labeling">
+      <div>Testing content here</div>
+    </CardCollapsibleContent>
+
+    <CardCollapsibleContent title="Collection Process">
+      <div>Testing content here</div>
+    </CardCollapsibleContent>
+
+    <CardCollapsibleContent
+      title="Inclusion Criteria-Accessibility in Data Collection"
+    >
+      <div>Testing content here</div>
+    </CardCollapsibleContent>
+
+    <CardCollapsibleContent title="Uses">
+      <div>Testing content here</div>
+    </CardCollapsibleContent>
+
+    <CardCollapsibleContent title="Dataset Distribution">
+      <div>Testing content here</div>
+    </CardCollapsibleContent>
+
+    <CardCollapsibleContent title="Maintenance">
+      <div>Testing content here</div>
+    </CardCollapsibleContent>
+  </div>
 </template>
