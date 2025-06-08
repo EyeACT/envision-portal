677a07d3ced4e2427e476072b1892868af82b494
Xuebin Dong <xdong@Xuebin-MacBook-Pro.local>
2025-06-08 16:41:15 -0700

    Add team page

---
+++ server/api/studies/[studyId]/metadata/team/index.get.ts
@@ -0,0 +1,38 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+export default defineEventHandler(async (event) => {
+  const { studyId } = event.context.params as { studyId: string };
+
+  if (!studyId) {
+    throw createError({ statusCode: 400, statusMessage: "Missing studyId" });
+  }
+
+  const sponsor = await prisma.studySponsors.findUnique({
+    where: { studyId },
+  });
+
+  return (
+    sponsor ?? {
+      studyId,
+      responsiblePartyType: null,
+      responsiblePartyInvestigatorGivenName: null,
+      responsiblePartyInvestigatorFamilyName: null,
+      responsiblePartyInvestigatorTitle: null,
+      responsiblePartyInvestigatorIdentifierScheme: null,
+      responsiblePartyInvestigatorIdentifierValue: null,
+      responsiblePartyInvestigatorAffiliationName: null,
+      responsiblePartyInvestigatorAffiliationIdentifier: null,
+      responsiblePartyInvestigatorAffiliationIdentifierScheme: null,
+      responsiblePartyInvestigatorAffiliationIdentifierSchemeUri: null,
+      leadSponsorName: null,
+      leadSponsorIdentifier: null,
+      leadSponsorIdentifierScheme: null,
+      leadSponsorIdentifierSchemeUri: null,
+      created: null,
+      updated: null,
+    }
+  );
+});
+
