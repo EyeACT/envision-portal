847e792f6e8285a0ad3e39edde6dcc4eb980e9b4
Sanjay Soundarajan <sanjay.16@live.com>
2025-06-16 11:49:51 -0700

    wip: add healthsheet metadata pages (#27)

---
+++ server/api/studies/[studyId]/datasets/[datasetId]/healthsheet/motivation/index.put.ts
@@ -0,0 +1,54 @@
+import { z } from "zod";
+
+const DatasetHealthsheetMotivationSchema = z.object({
+  records: z.array(
+    z.object({
+      id: z.number(),
+      question: z.string(),
+      response: z.string(),
+    }),
+  ),
+  version: z.number(),
+});
+
+export default defineEventHandler(async (event) => {
+  const session = await requireUserSession(event);
+
+  const { user } = session;
+  const userId = user.id;
+
+  const { datasetId, studyId } = event.context.params as {
+    datasetId: string;
+    studyId: string;
+  };
+
+  // Validate the request body
+  const body = await readValidatedBody(event, (b) =>
+    DatasetHealthsheetMotivationSchema.safeParse(b),
+  );
+
+  if (!body.success) {
+    throw createError({
+      statusCode: 400,
+      statusMessage: "Invalid  data",
+    });
+  }
+
+  const { records, version } = body.data;
+
+  const motivation = await prisma.datasetHealthsheet.update({
+    data: {
+      motivation: JSON.stringify({
+        records,
+        version,
+      }),
+    },
+    where: {
+      datasetId,
+    },
+  });
+
+  return {
+    success: true,
+  };
+});
