62755bf517f53f7ac3a47acb79ce2c75d967e01e
Sanjay Soundarajan <ssoundarajan@calmi2.org>
2025-06-18 02:56:05 -0700

    wip: added dataset file tree

--- pages/app/study/[studyId]/datasets/[datasetId]/files.vue
+++ pages/app/study/[studyId]/datasets/[datasetId]/files.vue
@@ -40,12 +40,10 @@ const items = ref([
           {
             icon: "i-vscode-icons-file-type-csv",
             label: "participant_001.csv",
-            trailingIcon: "i-heroicons-arrow-down-tray",
           },
           {
             icon: "i-vscode-icons-file-type-csv",
             label: "participant_002.csv",
-            trailingIcon: "i-heroicons-arrow-down-tray",
           },
         ],
         icon: "i-heroicons-user-group",
@@ -56,12 +54,10 @@ const items = ref([
           {
             icon: "i-vscode-icons-file-type-json",
             label: "baseline_survey.json",
-            trailingIcon: "i-heroicons-arrow-down-tray",
           },
           {
             icon: "i-vscode-icons-file-type-json",
             label: "followup_survey.json",
-            trailingIcon: "i-heroicons-arrow-down-tray",
           },
         ],
         defaultExpanded: true,
@@ -78,12 +74,10 @@ const items = ref([
       {
         icon: "i-vscode-icons-file-type-excel",
         label: "analysis_results.xlsx",
-        trailingIcon: "i-heroicons-arrow-down-tray",
       },
       {
         icon: "i-vscode-icons-file-type-csv",
         label: "summary_stats.csv",
-        trailingIcon: "i-heroicons-arrow-down-tray",
       },
     ],
     icon: "i-heroicons-cog-6-tooth",
@@ -92,47 +86,38 @@ const items = ref([
   {
     icon: "i-heroicons-document-text",
     label: "README.md",
-    trailingIcon: "i-heroicons-arrow-down-tray",
   },
   {
     icon: "i-heroicons-shield-check",
     label: "LICENSE.txt",
-    trailingIcon: "i-heroicons-arrow-down-tray",
   },
   {
     icon: "i-heroicons-clock",
     label: "CHANGELOG.md",
-    trailingIcon: "i-heroicons-arrow-down-tray",
   },
   {
     icon: "i-heroicons-heart",
     label: "healthsheet.md",
-    trailingIcon: "i-heroicons-arrow-down-tray",
   },
   {
     icon: "i-vscode-icons-file-type-json",
     label: "study_description.json",
-    trailingIcon: "i-heroicons-arrow-down-tray",
   },
   {
     icon: "i-vscode-icons-file-type-json",
     label: "dataset_description.json",
-    trailingIcon: "i-heroicons-arrow-down-tray",
   },
   {
     icon: "i-vscode-icons-file-type-csv",
     label: "participants.tsv",
-    trailingIcon: "i-heroicons-arrow-down-tray",
   },
   {
     icon: "i-vscode-icons-file-type-json",
     label: "participants.json",
-    trailingIcon: "i-heroicons-arrow-down-tray",
   },
   {
     icon: "i-vscode-icons-file-type-json",
     label: "dataset_structure_description.json",
-    trailingIcon: "i-heroicons-arrow-down-tray",
   },
 ]);
 </script>
