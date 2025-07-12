7e0076e22c693760aa550fad8d46124b93b6a839
Sanjay Soundarajan <ssoundarajan@calmi2.org>
2025-07-12 15:25:25 -0700

    wip: add changelog and readme review

---
+++ server/api/datasets/[datasetId]/publish/readme.get.ts
@@ -0,0 +1,34 @@
+export default defineEventHandler(async (event) => {
+  const session = await requireUserSession(event);
+
+  const { user } = session;
+  const userId = user.id;
+
+  const { datasetId } = event.context.params as {
+    datasetId: string;
+  };
+
+  // Get the dataset from the database
+  const dataset = await prisma.dataset.findUnique({
+    where: {
+      id: datasetId,
+      DatasetMember: {
+        some: {
+          userId,
+        },
+      },
+    },
+  });
+
+  // Check if the dataset exists
+  if (!dataset) {
+    throw createError({
+      statusCode: 404,
+      statusMessage: "Dataset not found",
+    });
+  }
+
+  return {
+    ...dataset,
+  };
+});
