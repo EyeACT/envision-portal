b2991bbd13c1e56953d4077604685a143109e946
Sanjay Soundarajan <ssoundarajan@calmi2.org>
2025-07-29 15:47:30 -0700

    wip: add publish script

--- server/api/datasets/[datasetId]/publish/index.post.ts
+++ server/api/datasets/[datasetId]/publish/index.post.ts
@@ -1,5 +1,7 @@
 import { DataLakeServiceClient } from "@azure/storage-file-datalake";
 import { createId } from "@paralleldrive/cuid2";
+import { faker } from "@faker-js/faker";
+import DatasetRecords from "~/dev/datasetRecords.json";
 
 export default defineEventHandler(async (event) => {
   const { AZURE_DRAFT_CONNECTION_STRING, AZURE_PUBLISHED_CONNECTION_STRING } =
@@ -65,25 +67,90 @@ export default defineEventHandler(async (event) => {
   for await (const draftFile of draftFileSystemClient.listPaths({
     recursive: true,
   })) {
-    if (draftFile.isDirectory) continue;
+    const filePath = draftFile.name || "";
 
-    const draftFileClient = draftFileSystemClient.getFileClient(draftFile.name);
+    if (filePath === "" || draftFile.isDirectory) {
+      continue;
+    }
+
+    const draftFileClient = draftFileSystemClient.getFileClient(filePath);
     const content = await draftFileClient.readToBuffer();
 
-    const publishedFileClient = publishedFileSystemClient.getFileClient(
-      draftFile.name,
-    );
+    const publishedFileClient =
+      publishedFileSystemClient.getFileClient(filePath);
 
     await publishedFileClient.create();
-    await publishedFileClient.append(content, 0, content.length);
-    await publishedFileClient.flush(content.length);
+    await publishedFileClient.upload(content);
   }
 
+  const firstEntry = DatasetRecords[0];
+
+  const datasetDescription = JSON.stringify(
+    firstEntry.publishedMetadata.datasetDescription,
+    null,
+    2,
+  );
+  const healthsheet = JSON.stringify(
+    firstEntry.publishedMetadata.healthsheet,
+    null,
+    2,
+  );
+  const readme = JSON.stringify(firstEntry.publishedMetadata.readme, null, 2);
+  const studyDescription = JSON.stringify(
+    firstEntry.publishedMetadata.studyDescription,
+    null,
+    2,
+  );
+
+  const changelog = "# Changelog\n\n## 1.0.0\n\n- Initial release";
+
   // 8. Generate and upload the dataset metadata file
+
+  let publishedFileClient = publishedFileSystemClient.getFileClient(
+    "dataset_description.json",
+  );
+  let data: Buffer = Buffer.from(datasetDescription);
+
+  await publishedFileClient.create();
+  await publishedFileClient.upload(data);
+
   // 9. Generate and upload the study metadata file
+
+  publishedFileClient = publishedFileSystemClient.getFileClient(
+    "study_description.json",
+  );
+  data = Buffer.from(studyDescription);
+
+  await publishedFileClient.create();
+  await publishedFileClient.upload(data);
+
   // 10. Generate and upload the healthsheet file
+
+  publishedFileClient =
+    publishedFileSystemClient.getFileClient("healthsheet.md");
+
+  data = Buffer.from(healthsheet);
+
+  await publishedFileClient.create();
+  await publishedFileClient.upload(data);
+
   // 11. Generate and upload the changelog file
+
+  publishedFileClient = publishedFileSystemClient.getFileClient("CHANGELOG.md");
+
+  data = Buffer.from(changelog);
+
+  await publishedFileClient.create();
+  await publishedFileClient.upload(data);
+
   // 12. Generate and upload the readme file
+
+  publishedFileClient = publishedFileSystemClient.getFileClient("README.md");
+
+  data = Buffer.from(readme);
+
+  await publishedFileClient.create();
+  await publishedFileClient.upload(data);
 
   // Register the doi for the dataset
 
@@ -92,16 +159,16 @@ export default defineEventHandler(async (event) => {
     data: {
       title: dataset.title,
       canonicalId: dataset.canonicalId,
-      data: {},
-      datasetId,
+      data: firstEntry.data,
+      datasetId: createId(),
       description: dataset.description,
-      doi: "10.1000/envision.a3sv45sd",
+      doi: `10.1000/envision.${faker.string.alphanumeric(10)}`,
       external: false,
       externalUrl: null,
-      files: {},
-      publishedMetadata: {},
-      studyTitle: dataset.title,
-      versionTitle: dataset.version || "",
+      files: firstEntry.files,
+      publishedMetadata: firstEntry.publishedMetadata,
+      studyTitle: firstEntry.studyTitle,
+      versionTitle: faker.system.semver() || "",
     },
   });
 
