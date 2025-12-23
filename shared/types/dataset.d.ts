2afbde54a45940fc9b7ed005b6ed0299df5f7f0a
Dorian Portillo <wheresdorian@gmail.com>
2025-12-23 11:20:44 -0800

    feat: ✨ Discovery tag (#50)
    * feat: enhance dataset discovery with new data structure and registration details
    
    * feat: integrate @internationalized/date for enhanced date range filtering in datasets
    
    ---------
    
    Co-authored-by: Sanjay Soundarajan <ssoundarajan@calmi2.org>

--- shared/types/dataset.d.ts
+++ shared/types/dataset.d.ts
@@ -8,3 +8,35 @@ export interface DatasetHealthsheetRecords {
   version: number;
   records: DatasetHealthsheetRecord[];
 }
+
+interface DiscoveryListDatasetCreator {
+  creatorName: string;
+  nameType: string;
+  affiliation: {
+    affiliationName: string;
+  }[];
+  nameIdentifier: {
+    nameIdentifierValue: string;
+    nameIdentifierScheme: string;
+    schemeURI?: string;
+  }[];
+}
+
+export interface DiscoveryDatasetList {
+  id: number;
+  canonicalId: string;
+  datasetId: string;
+  doi: string;
+  external: boolean;
+  title: string;
+  description: string;
+  versionTitle: string;
+  rights: string[];
+  registrationSource: string;
+  creators: DiscoveryListDatasetCreator[];
+  keywords: string[];
+  created: Date;
+  versionCount: number;
+  labelingMethod: string;
+  validationInfo: string;
+}
