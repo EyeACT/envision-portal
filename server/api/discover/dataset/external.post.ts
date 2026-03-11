2c35b1a7802629a5a74a596969d5e045459db985
Sanjay Soundarajan <ssoundarajan@calmi2.org>
2026-03-10 19:16:39 -0700

    feat: add external datasets addition endpoint

---
+++ server/api/discover/dataset/external.post.ts
@@ -0,0 +1,119 @@
+import { z } from "zod";
+
+const fileSchema: z.ZodType = z.lazy(() =>
+  z.array(
+    z.union([
+      z.object({
+        label: z.string(),
+        children: z.array(fileSchema),
+      }),
+      z.object({
+        name: z.string(),
+      }),
+    ]),
+  ),
+);
+
+const datasetSchema = z.object({
+  title: z.string(),
+  created: z.string(),
+  data: z.any(),
+  datasetId: z.string(),
+  canonicalId: z.string(),
+  description: z.string(),
+  doi: z.string().optional(),
+  externalUrl: z.string(),
+  files: fileSchema,
+  publishedMetadata: z.any(),
+  studyTitle: z.string(),
+  updated: z.string(),
+  versionTitle: z.string(),
+  PublishedDatasetRegistrationDetails: z.object({
+    datasetSource: z.string(),
+    extractionMethod: z.string(),
+    extractionVersion: z.string(),
+  }),
+});
+
+export default defineEventHandler(async (event) => {
+  // Verify the authentication key from the headers and return a 401 error if it is invalid
+
+  const AuthenticationKey = getRequestHeader(event, "x-api-key");
+
+  if (AuthenticationKey !== process.env.EXTERNAL_API_KEY) {
+    throw createError({
+      statusCode: 401,
+      statusMessage: "Unauthorized",
+    });
+  }
+
+  const body = await readValidatedBody(event, (b) =>
+    datasetSchema.safeParse(b),
+  );
+
+  if (!body.success) {
+    console.log("Validation error:", body.error);
+    throw createError({
+      statusCode: 400,
+      statusMessage: "Invalid request body",
+    });
+  }
+
+  const externalUrl = body.data.externalUrl.toLowerCase();
+
+  // check if the dataset with the same externalUrl already exists in the database and return a 400 error if it does
+  const existingDataset = await prisma.publishedDataset.findFirst({
+    where: {
+      externalUrl,
+    },
+  });
+
+  if (existingDataset) {
+    throw createError({
+      statusCode: 400,
+      statusMessage: "Dataset with the same externalUrl already exists",
+    });
+  }
+
+  // Create a new dataset in the database. We will get the generated id and use it to create the canonicalId in the format "external-{id}"
+  const newDataset = await prisma.publishedDataset.create({
+    data: {
+      title: body.data.title,
+      created: new Date(parseInt(body.data.created) * 1000),
+      data: body.data.data,
+      description: body.data.description,
+      doi: body.data.doi,
+      external: true,
+      externalUrl: body.data.externalUrl,
+      files: body.data.files,
+      publishedMetadata: body.data.publishedMetadata,
+      studyTitle: body.data.studyTitle,
+      updated: new Date(parseInt(body.data.created) * 1000),
+      versionTitle: body.data.versionTitle,
+      PublishedDatasetRegistrationDetails: {
+        create: {
+          datasetSource:
+            body.data.PublishedDatasetRegistrationDetails.datasetSource,
+          extractionMethod:
+            body.data.PublishedDatasetRegistrationDetails.extractionMethod,
+          extractionVersion:
+            body.data.PublishedDatasetRegistrationDetails.extractionVersion,
+        },
+      },
+    },
+  });
+
+  // Update the canonicalId of the dataset to be in the format "external-{id}"
+  await prisma.publishedDataset.update({
+    where: { id: newDataset.id },
+    data: {
+      canonicalId: newDataset.id.toString(),
+      datasetId: newDataset.id.toString(),
+    },
+  });
+
+  return {
+    statusCode: 201,
+    statusMessage: "Request submitted",
+  };
+});
