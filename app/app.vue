680abf177c30407764200cfdfd2659ba25536fed
Sanjay Soundarajan <ssoundarajan@calmi2.org>
2026-05-05 18:10:40 -0700

    feat: add favicon

--- app/app.vue
+++ app/app.vue
@@ -11,7 +11,17 @@ useHead({
   htmlAttrs: {
     lang: "en",
   },
-  link: [{ href: "/favicon.ico", rel: "icon" }],
+  link: [
+    { href: "/favicon.ico", rel: "shortcut icon" },
+    { href: "/favicon-96x96.png", rel: "icon", sizes: "96x96" },
+    { href: "/favicon.svg", rel: "icon", type: "image/svg+xml" },
+    {
+      href: "/apple-touch-icon.png",
+      rel: "apple-touch-icon",
+      sizes: "180x180",
+    },
+    { href: "/site.webmanifest", rel: "manifest" },
+  ],
   meta: [
     { charset: "utf-8" },
     { name: "viewport", content: "width=device-width, initial-scale=1" },
